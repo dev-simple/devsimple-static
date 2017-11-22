443eb555136013f183cd89321a48b48e660c0393
Anikin Artyom <anikinartyom@gmail.com>
2017-11-22 19:02:10 +0500

    Update header

--- helpers/application_helpers.rb
+++ helpers/application_helpers.rb
@@ -4,4 +4,13 @@ module ApplicationHelpers
     raise ArgumentError unless result.any?
     result.first
   end
+
+  def svg(name)
+    root = Middleman::Application.root
+    images_path = config[:images_dir]
+    file_path = "#{root}/source/#{images_path}/#{name}.svg"
+
+    return File.read(file_path) if File.exists?(file_path)
+    "(SVG not found)"
+  end
 end
