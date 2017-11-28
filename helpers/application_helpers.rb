a56312813492dd2c4ddb7e6937be1df9cb9e49b0
Anikin Artyom <anikinartyom@gmail.com>
2017-11-28 19:08:20 +0500

    Fix style code

--- helpers/application_helpers.rb
+++ helpers/application_helpers.rb
@@ -1,6 +1,6 @@
 module ApplicationHelpers
   def find_author(author)
-    result = data.authors.select {|a| a.keys.first == author}
+    result = data.authors.select { |a| a.keys.first == author }
     raise ArgumentError unless result.any?
     result.first
   end
@@ -10,8 +10,8 @@ module ApplicationHelpers
     images_path = config[:images_dir]
     file_path = "#{root}/source/#{images_path}/#{name}.svg"
 
-    return File.read(file_path) if File.exists?(file_path)
-    "(SVG not found)"
+    return File.read(file_path) if File.exist?(file_path)
+    '(SVG not found)'
   end
 
   def video(link)
